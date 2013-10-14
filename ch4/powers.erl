b2be45d5712130ffc39a65cfdd82a7c2e5c284dd
Angus Macdonald <amacdonald@aetherworks.com>
2013-10-14 15:59:52 -0400

    4-5 Completed.

--- ch4/powers.erl
+++ ch4/powers.erl
@@ -3,7 +3,7 @@
 %% @version 0.1
 
 -module(powers).
--export([raise/2]).
+-export([raise/2, nth_root/2]).
 
 %% @doc Raises a number to the given power.
 -spec(raise(number(), integer()) -> number()).
@@ -13,3 +13,21 @@ raise(X, N) when N > 0 -> raise (X, N, 1).
 
 raise(_, 0, Acc) -> Acc;
 raise(X, N, Acc) -> raise(X, N-1, X * Acc).
+
+%% @doc Find the nth root of a number.
+-spec(nth_root(number(), integer()) -> number()).
+nth_root(X, N) -> nth_root(X, N, X / 2.0).
+
+nth_root(X, N, A) ->
+	io:format("Guess: ~p~n", [A]),
+	F = raise(A, N) - X,
+	Fprime = N * raise(A, N-1),
+	Next = A - F / Fprime,
+	Change = abs(Next - A),
+
+	if
+		Change < 1.0e-8 ->
+			Next;
+		true ->
+			nth_root(X, N, Next)
+	end.
