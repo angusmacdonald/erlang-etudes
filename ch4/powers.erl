cda11562eb7116de436c2643d9e39bb63e6b1b11
Angus Macdonald <amacdonald@aetherworks.com>
2013-10-14 15:49:32 -0400

    4-4 Commenting & Spec

--- ch4/powers.erl
+++ ch4/powers.erl
@@ -1,6 +1,12 @@
+%% @author Angus Macdonald (amacdonald AT aetherworks.com)
+%% @doc Functions using recursion.
+%% @version 0.1
+
 -module(powers).
 -export([raise/2]).
 
+%% @doc Raises a number to the given power.
+-spec(raise(number(), integer()) -> number()).
 raise(_, 0) -> 1;
 raise(X, N) when N < 0 -> 1.0 / raise(X, -N);
 raise(X, N) when N > 0 -> raise (X, N, 1).
